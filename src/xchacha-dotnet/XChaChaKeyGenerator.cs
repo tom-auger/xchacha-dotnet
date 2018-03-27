ea1ef2d8c10315c97a84b4434fa34914c057b483
Tom Auger <tomauger@live.com>
2018-03-27 16:40:11 +0100

    Use static usings in key generator

--- src/xchacha-dotnet/XChaChaKeyGenerator.cs
+++ src/xchacha-dotnet/XChaChaKeyGenerator.cs
@@ -1,13 +1,14 @@
 namespace XChaChaDotNet
 {
     using System;
+    using static SodiumInterop;
 
     public static class XChaChaKeyGenerator
     {
         public static ReadOnlySpan<byte> GenerateKey() 
         {
-            var keyBytes = new byte[SodiumInterop.crypto_secretstream_xchacha20poly1305_KEYBYTES];
-            SodiumInterop.crypto_secretstream_xchacha20poly1305_keygen(keyBytes);
+            var keyBytes = new byte[crypto_secretstream_xchacha20poly1305_KEYBYTES];
+            crypto_secretstream_xchacha20poly1305_keygen(keyBytes);
             return keyBytes.AsReadOnlySpan();
         }
     }
