6a8ad4bea54a419af9e08a3c8baa3e7aa6e430c0
Tom Auger <tomauger@live.com>
2018-03-30 20:52:34 +0100

    Add tests for VerifyEndOfCipherStream

--- tests/xchacha-dotnet.unittests/XChaChaDecryptionStreamTests.cs
+++ tests/xchacha-dotnet.unittests/XChaChaDecryptionStreamTests.cs
@@ -34,6 +34,58 @@ namespace XChaChaDotNet.UnitTests
         }
 
         [Fact]
+        public void Test_Decrypt_VerifyEndOfStream_FalseWhenPartiallyDecrypted()
+        {
+            using (var cipherTextStream = new MemoryStream())
+            {
+                var key = XChaChaKeyGenerator.GenerateKey();
+                var plainText = new byte[1024 * 1024];
+                Array.Fill(plainText, (byte)0x7);
+
+                using (var cipherStream = new XChaChaEncryptionStream(cipherTextStream, key))
+                {
+                    cipherStream.Write(plainText, 0, plainText.Length);
+                }
+
+                cipherTextStream.Position = 0;
+
+                using (var decryptionStream = new XChaChaDecryptionStream(cipherTextStream, key))
+                {
+                    var decryptedPlainText = new byte[plainText.Length];
+
+                    decryptionStream.Read(decryptedPlainText, 0, decryptedPlainText.Length / 2);
+                    Assert.False(decryptionStream.VerifyEndOfCipherStream());
+                }
+            }
+        }
+
+        [Fact]
+        public void Test_Decrypt_VerifyEndOfStream_TrueWhenFullyDecrypted()
+        {
+            using (var cipherTextStream = new MemoryStream())
+            {
+                var key = XChaChaKeyGenerator.GenerateKey();
+                var plainText = new byte[1024 * 1024];
+                Array.Fill(plainText, (byte)0x7);
+
+                using (var cipherStream = new XChaChaEncryptionStream(cipherTextStream, key))
+                {
+                    cipherStream.Write(plainText, 0, plainText.Length);
+                }
+
+                cipherTextStream.Position = 0;
+
+                using (var decryptionStream = new XChaChaDecryptionStream(cipherTextStream, key))
+                {
+                    var decryptedPlainText = new byte[plainText.Length];
+
+                    decryptionStream.Read(decryptedPlainText, 0, decryptedPlainText.Length);
+                    Assert.True(decryptionStream.VerifyEndOfCipherStream());
+                }
+            }
+        }
+
+        [Fact]
         public void Test_Decrypt_OverReadDecryptionStream_OutputsCorrectNumberOfBytes()
         {
             using (var cipherTextStream = new MemoryStream())
