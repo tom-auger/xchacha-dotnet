152a9f7262de3ee8c5a9d7b6676b042d5e286e2b
Tom Auger <tomauger@live.com>
2018-03-31 15:14:52 +0100

    Add benchmarks project with simple encryption stream test

---
+++ tests/xchacha-dotnet.benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace XChaChaDotNet.Benchmarks
+{
+    using BenchmarkDotNet.Running;
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<EncryptionStreamTest>();
+        }
+    }
+}
---
+++ tests/xchacha-dotnet.benchmarks/Tests/EncryptionStreamTest.cs
@@ -0,0 +1,34 @@
+namespace XChaChaDotNet.Benchmarks
+{
+    using BenchmarkDotNet.Attributes;
+    using System;
+    using XChaChaDotNet;
+
+    public class EncryptionStreamTest
+    {
+        private const int N = 1000;
+        private readonly byte[] data;
+
+        public EncryptionStreamTest()
+        {
+            this.data = new byte[N];
+            new Random(31).NextBytes(data);
+        }
+
+        [Benchmark]
+        public long RunTest()
+        {
+            var key = XChaChaKeyGenerator.GenerateKey();
+
+            using (var devNull = new DevNullStream())
+            {
+                using (var encryptionStream = new XChaChaEncryptionStream(devNull, key))
+                {
+                    encryptionStream.Write(data);
+                }
+
+                return devNull.Length;
+            }
+        }
+    }
+}
