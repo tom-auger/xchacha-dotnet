db80583555b95444a5fd60f5b051ff0ed413b45a
Tom Auger <tomauger@live.com>
2018-04-15 19:14:34 +0100

    Test all derived types in StreamBase tests

--- tests/unittests/XChaChaStream/XChaChaStreamBaseTests.cs
+++ tests/unittests/XChaChaStream/XChaChaStreamBaseTests.cs
@@ -7,88 +7,110 @@ namespace XChaChaDotNet.UnitTests
 
     public class XChaChaStreamBaseTests
     {
-        [Fact]
-        public void Test_Write_NullBuffer_ExceptionThrown()
+        public static readonly TheoryData<Type> StreamTypes = new TheoryData<Type>
+        {
+            typeof(XChaChaStream),
+            typeof(XChaChaBufferedStream)
+        };
+
+        public static readonly TheoryData<Type, EncryptionMode> StreamsAndEncryptionModes = new TheoryData<Type, EncryptionMode>
+        {
+            { typeof(XChaChaStream), EncryptionMode.Encrypt },
+            { typeof(XChaChaStream), EncryptionMode.Decrypt },
+            { typeof(XChaChaBufferedStream), EncryptionMode.Encrypt },
+            { typeof(XChaChaBufferedStream), EncryptionMode.Decrypt }
+        };
+
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Write_NullBuffer_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Write(null, 0, 1);
                 Assert.Throws<ArgumentNullException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Write_OffsetNegative_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Write_OffsetNegative_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Write(Array.Empty<byte>(), -1, 1);
                 Assert.Throws<ArgumentOutOfRangeException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Write_CountNegative_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Write_CountNegative_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Write(Array.Empty<byte>(), 0, -1);
                 Assert.Throws<ArgumentOutOfRangeException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Write_ParametersInconsistent_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Write_ParametersInconsistent_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Write(Array.Empty<byte>(), 3, 1);
                 Assert.Throws<ArgumentException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Read_NullBuffer_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Read_NullBuffer_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Read(null, 0, 1);
                 Assert.Throws<ArgumentNullException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Read_OffsetNegative_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Read_OffsetNegative_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Read(Array.Empty<byte>(), -1, 1);
                 Assert.Throws<ArgumentOutOfRangeException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Read_CountNegative_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Read_CountNegative_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Read(Array.Empty<byte>(), 0, -1);
                 Assert.Throws<ArgumentOutOfRangeException>(action);
             }
         }
 
-        [Fact]
-        public void Test_Read_ParametersInconsistent_ExceptionThrown()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Read_ParametersInconsistent_ExceptionThrown(Type streamType)
         {
             using (var key = XChaChaKey.Generate())
-            using (var xchachaStream = new XChaChaBufferedStream(Stream.Null, key, EncryptionMode.Encrypt))
+            using (var xchachaStream = (XChaChaStreamBase)Activator.CreateInstance(streamType, Stream.Null, key, EncryptionMode.Encrypt, false))
             {
                 Action action = () => xchachaStream.Read(Array.Empty<byte>(), 3, 1);
                 var exception = Assert.Throws<ArgumentException>(action);
@@ -97,24 +119,31 @@ namespace XChaChaDotNet.UnitTests
         }
 
         [Theory]
-        [InlineData(EncryptionMode.Encrypt)]
-        [InlineData(EncryptionMode.Decrypt)]
-        public void Test_Initialize_KeyNull_ThrowsNullArgumentException(EncryptionMode mode)
+        [MemberData(nameof(StreamsAndEncryptionModes))]
+        public void Test_Initialize_KeyNull_ThrowsNullArgumentException(Type streamType, EncryptionMode mode)
         {
-            Action action = () => new XChaChaBufferedStream(Stream.Null, null, mode);
-            Assert.Throws<ArgumentNullException>(action);
+            Action action = () => Activator.CreateInstance(streamType, Stream.Null, null, mode, false);
+            var exception = Assert.ThrowsAny<Exception>(action);
+            // The action throws a System.Reflection.TargetInvocationException. The inner exception contains
+            // the actual exception thrown in the class constructor
+            Assert.IsType<ArgumentNullException>(exception.InnerException);
         }
 
-        [Fact]
-        public void Test_Initialize_Decrypt_WrongHeaderLength_ThrowsCryptographicException()
+        [Theory]
+        [MemberData(nameof(StreamTypes))]
+        public void Test_Initialize_Decrypt_WrongHeaderLength_ThrowsCryptographicException(Type streamType)
         {
             var invalidHeader = new byte[3];
             using (var key = XChaChaKey.Generate())
             using (var ciphertextStream = new MemoryStream(invalidHeader))
             {
-                Action action = () => new XChaChaBufferedStream(ciphertextStream, key, EncryptionMode.Decrypt);
-                var exception = Assert.Throws<CryptographicException>(action);
-                Assert.Equal("invalid or corrupt header", exception.Message);
+                Action action = () => Activator.CreateInstance(streamType, ciphertextStream, key, EncryptionMode.Decrypt, false);
+                var exception = Assert.ThrowsAny<Exception>(action);
+                // The action throws a System.Reflection.TargetInvocationException. The inner exception contains
+                // the actual exception thrown in the class constructor
+                var innerException = exception.InnerException;
+                Assert.IsType<CryptographicException>(innerException);
+                Assert.Equal("invalid or corrupt header", innerException.Message);
             }
         }
     }
