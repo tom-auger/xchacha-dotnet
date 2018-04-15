aa54e8b03dba87692758d9c383fa893dc3d3a516
Tom Auger <tomauger@live.com>
2018-04-15 10:48:18 +0100

    Synchronize calls to Sodium initialize
    Had several randomly occuring exceptions thrown by
    AssemblyLoadContext.ResolveUnamangedDll to do with
    concurrently inserting keys into a dictionary.
    This change seems to prevent this from happening.

--- src/Sodium/Sodium.cs
+++ src/Sodium/Sodium.cs
@@ -1,23 +1,32 @@
 namespace XChaChaDotNet
 {
+    using System;
     using System.Security.Cryptography;
     using static SodiumInterop;
 
     internal static class Sodium
     {
+        [ThreadStatic]
         private static bool initialized;
+        private static object lockObject = new object();
 
         public static void Initialize()
         {
             if (!initialized)
             {
-                // sodium_init returns 0 on success, -1 on failure and 1 if it's already been initialized
-                if (sodium_init() < 0)
+                lock (lockObject)
                 {
-                    throw new CryptographicException("sodium initialization failed");
-                }
+                    if (!initialized)
+                    {
+                        // sodium_init returns 0 on success, -1 on failure and 1 if it's already been initialized
+                        if (sodium_init() < 0)
+                        {
+                            throw new CryptographicException("sodium initialization failed");
+                        }
 
-                initialized = true;
+                        initialized = true;
+                    }
+                }
             }
         }
     }
