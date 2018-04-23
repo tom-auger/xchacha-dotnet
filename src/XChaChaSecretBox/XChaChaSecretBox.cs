10b6705c3e45b2b768da34ff664f87a81e2d4912
Tom Auger <tomauger@live.com>
2018-04-23 08:15:25 +0100

    Add documentation comments

--- src/XChaChaSecretBox/XChaChaSecretBox.cs
+++ src/XChaChaSecretBox/XChaChaSecretBox.cs
@@ -5,15 +5,29 @@ namespace XChaChaDotNet
     using System.Security.Cryptography;
     using static SodiumInterop;
 
+    /// <summary>
+    /// Represents an XChaCha secret box. Recommended for encrypting a single message
+    /// with a key and nonce to keep it confidential.
+    /// </summary>
     public class XChaChaSecretBox
     {
         private readonly XChaChaKey key;
 
+        /// <summary>
+        /// Creates an instance using the specified key.
+        /// </summary>
+        /// <param name="key">The key to use for encryption/decryption.</param>
         public XChaChaSecretBox(XChaChaKey key)
         {
             this.key = key;
         }
 
+        /// <summary>
+        /// Encrypts a <paramref name="message"/> and writes the output to <paramref name="ciphertext"/>.
+        /// </summary>
+        /// <param name="message">The message to encrypt.</param>
+        /// <param name="ciphertext">The buffer in which to write the ciphertext.</param>
+        /// <param name="nonce">The nonce to use when encrypting the <paramref name="message"/>.</param>
         public void Encrypt(ReadOnlySpan<byte> message, Span<byte> ciphertext, XChaChaNonce nonce)
         {
             ValidateEncryptParameters(message, ciphertext, nonce);
@@ -26,6 +40,14 @@ namespace XChaChaDotNet
                 this.key.Handle);
         }
 
+        /// <summary>
+        /// Decrypts a <paramref name="ciphertext"/>, verifies the authenticaion tag, and if successful,
+        /// writes the output to <paramref name="message"/>.
+        /// </summary>
+        /// <param name="ciphertext">The ciphertext to decrypt.</param>
+        /// <param name="message">The buffer in which to write the decrypted message.</param>
+        /// <param name="nonce">The nonce to use when decrypting the <paramref name="ciphertext"/>.</param>
+        /// <returns>Whether the decryption succeeded.</returns>
         public bool Decrypt(ReadOnlySpan<byte> ciphertext, Span<byte> message, XChaChaNonce nonce)
         {
             ValidateDecryptParameters(ciphertext, message, nonce);
@@ -41,6 +63,11 @@ namespace XChaChaDotNet
             return returnCode == 0;
         }
 
+        /// <summary>
+        /// Calculates the size of the ciphertext for a given plaintext length.
+        /// </summary>
+        /// <param name="plaintextLength">The length of the plaintext.</param>
+        /// <returns>The length of the ciphertext.</returns>
         public static int GetCipherTextLength(int plaintextLength) =>
             plaintextLength + crypto_secretbox_xchacha20poly1305_MACBYTES;
 
--- src/XChaChaSecrets/XChaChaNonce.cs
+++ src/XChaChaSecrets/XChaChaNonce.cs
@@ -4,10 +4,17 @@ namespace XChaChaDotNet
     using System.Runtime.InteropServices;
     using static SodiumInterop;
 
+    /// <summary>
+    /// Represents a nonce for use with XChaCha ciphers.
+    /// </summary>
     public readonly ref struct XChaChaNonce
     {
         private readonly ReadOnlySpan<byte> bytes;
 
+        /// <summary>
+        /// Creates an instance from an existing nonce.
+        /// </summary>
+        /// <param name="nonceBytes">The raw nonce bytes.</param>
         public XChaChaNonce(ReadOnlySpan<byte> nonceBytes)
         {
             if (nonceBytes.Length != crypto_secretbox_xchacha20poly1305_NONCEBYTES)
@@ -16,12 +23,19 @@ namespace XChaChaDotNet
             this.bytes = nonceBytes;
         }
 
+        /// <summary>
+        /// Returns the raw nonce bytes.
+        /// </summary>
         public ReadOnlySpan<byte> ReadOnlySpan => this.bytes;
 
         internal ref byte Handle => ref MemoryMarshal.GetReference(this.bytes);
 
         internal bool IsEmpty => this.bytes == ReadOnlySpan<byte>.Empty;
 
+        /// <summary>
+        /// Creates an instance with a new randomly generated nonce.
+        /// </summary>
+        /// <returns>An instance with a new randomly generated nonce.</returns>
         public static XChaChaNonce Generate()
         {
             Sodium.Initialize();
