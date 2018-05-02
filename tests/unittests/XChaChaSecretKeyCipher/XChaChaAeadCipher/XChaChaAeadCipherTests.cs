101b4c4ea79acdfda1febeaafcc64575565f4a88
Tom Auger <tomauger@live.com>
2018-05-02 12:15:57 +0100

    Add some aead tests with additional data

--- tests/unittests/XChaChaSecretKeyCipher/XChaChaAeadCipher/XChaChaAeadCipherTests.cs
+++ tests/unittests/XChaChaSecretKeyCipher/XChaChaAeadCipher/XChaChaAeadCipherTests.cs
@@ -27,11 +27,27 @@ namespace XChaChaDotNet
                 Assert.False(ciphertext.All(b => b == 0));
             }
         }
+
+        [Fact]
+        public void Test_Encrypt_WithAdditionalData_ReturnsNonZeroOutput()
+        {
+            using (var key = XChaChaKey.Generate())
+            {
+                var aeadCipher = new XChaChaAeadCipher();
+                var nonce = XChaChaNonce.Generate();
+                var additionalData = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+
+                var message = RandomBytesGenerator.NextBytes(1024 * 1024);
+                var ciphertext = aeadCipher.Encrypt(message, key, nonce, additionalData);
+
+                Assert.False(ciphertext.All(b => b == 0));
+            }
+        }
         #endregion
 
         #region Decryption
         [Fact]
-        public void Test_Decrypt_WithAdditionalData()
+        public void Test_Decrypt_WithAdditionalData_Success()
         {
             using (var key = XChaChaKey.Generate())
             {
@@ -45,6 +61,27 @@ namespace XChaChaDotNet
 
                 var result = aeadCipher.Decrypt(ciphertext, key, nonce, additionalData);
                 Assert.Equal(message, result);
+            }
+        }
+
+        [Fact]
+        public void Test_Decrypt_WithInvalidAdditionalData_Fails()
+        {
+            using (var key = XChaChaKey.Generate())
+            {
+                var aeadCipher = new XChaChaAeadCipher();
+                var nonce = XChaChaNonce.Generate().ToArray();
+                var additionalData = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+
+                const int messageLength = 1024 * 1024;
+                var message = RandomBytesGenerator.NextBytes(messageLength);
+                var ciphertext = aeadCipher.Encrypt(message, key, new XChaChaNonce(nonce), additionalData);
+
+                var invalidAdditionalData = Encoding.UTF8.GetBytes("banana");
+                Action action = () => { aeadCipher.Decrypt(ciphertext, key, new XChaChaNonce(nonce), invalidAdditionalData); };
+                var exception = Assert.Throws<CryptographicException>(action);
+                
+                Assert.Equal("decryption failed", exception.Message);
             }
         }
         #endregion
